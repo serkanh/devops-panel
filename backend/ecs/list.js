ab2d36f905af0de07d02613930bd8e01f1e9ec68
serkan <shaytac@gmail.com>
2019-04-18 13:50:05 -0400

    update .gitingore

---
+++ backend/ecs/list.js
@@ -0,0 +1,14 @@
+'use strict';
+import ECS from 'aws-sdk/clients/ecs'
+
+const ecs = new ECS({region:'us-east-1'})
+
+export async function main(event, context) {
+	let clusters = await ecs.listClusters().promise()
+	console.log(clusters.clusterArns)
+  return {
+    statusCode: 200,
+    body: JSON.stringify({...clusters.clusterArns})
+
+	};
+}
