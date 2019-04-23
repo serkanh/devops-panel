16f1457acf7cbfa3b3b6b0aa7cdac013da96daa5
serkan <shaytac@gmail.com>
2019-04-23 10:20:10 -0400

    return combined results

--- backend/ecs/get.js
+++ backend/ecs/get.js
@@ -1,13 +1,13 @@
 "use strict";
 import ECS from "aws-sdk/clients/ecs";
-import Elbv2 from "aws-sdk/clients/elbv2";
-import Elb from "aws-sdk/clients/elb";
-
+import ELBV2 from "aws-sdk/clients/elbv2";
+import ELB from "aws-sdk/clients/elb";
+import EC2 from "aws-sdk/clients/ec2";
 
 const ecs = new ECS({ region: "us-east-1" });
-const elbv2 = new Elbv2({ region: "us-east-1" });
-const elb = new Elb({ region: "us-east-1" });
-
+const elbv2 = new ELBV2({ region: "us-east-1" });
+const elb = new ELB({ region: "us-east-1" });
+const ec2 = new EC2({ region: "us-east-1" });
 /**
  *
  * @param {string} clusterName
@@ -15,53 +15,57 @@ const elb = new Elb({ region: "us-east-1" });
  */
 async function listServices(clusterName) {
   try {
-		let services = await ecs.listServices({cluster:clusterName}).promise()
-		return services.serviceArns.map((service)=>service.split('/')[1])
+    let services = await ecs.listServices({ cluster: clusterName }).promise();
+    return services.serviceArns.map(service => service.split("/")[1]);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
 }
-
 
 /**
  *
  * @param {string} targetGroupArn
- * @return {Object}
+ * @return {string} alb dns name
  */
-async function getTargetGroupAlb(targetGroupArn){
-	if (targetGroupArn === undefined){
-		return null
-	}
-	try {
-		let albArn = await elbv2.describeTargetGroups({TargetGroupArns:[targetGroupArn]}).promise()
-		let alb = await elbv2.describeLoadBalancers({LoadBalancerArns:albArn.TargetGroups[0].LoadBalancerArns}).promise()
-		//console.log(alb.LoadBalancers[0].DNSName)
-		return alb.LoadBalancers[0].DNSName
-	} catch (error) {
-		throw new Error(error)
-	}
+async function getTargetGroupAlb(targetGroupArn) {
+  if (targetGroupArn === undefined) {
+    return null;
+  }
+  try {
+    let albArn = await elbv2
+      .describeTargetGroups({ TargetGroupArns: [targetGroupArn] })
+      .promise();
+    let alb = await elbv2
+      .describeLoadBalancers({
+        LoadBalancerArns: albArn.TargetGroups[0].LoadBalancerArns
+      })
+      .promise();
+
+    return alb.LoadBalancers[0].DNSName;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
+/**
+ *
+ * @param {Object} elbObj
+ * @return {string} elb dns name
+ */
+async function getElbDnsName(elbObj) {
+  if (elbObj[0].loadBalancerName === undefined) {
+    return null;
+  }
+  try {
 
-async function getElbDnsName(elbObj){
-	//console.log(elbObj)
-	if (elbObj[0].loadBalancerName === undefined){
-		return null
-	}
-  const name = elbObj[0].loadBalancerName
-  try{
-		let lbnames = [ `${name}` ]
-	//	console.log(`lbnames ======> ${lbnames}`)
-
-		let elasticlb = await elb.describeLoadBalancers({LoadBalancerNames:lbnames}).promise()
-		//console.log(elasticlb)
-		//console.log(elasticlb)
-		return elasticlb.LoadBalancerDescriptions[0].DNSName;
-	} catch (error) {
-		console.log(error)
-	}
+    let elasticlb = await elb
+      .describeLoadBalancers({ LoadBalancerNames:  [elbObj[0].loadBalancerName] })
+      .promise();
+    return elasticlb.LoadBalancerDescriptions[0].DNSName;
+  } catch (error) {
+    console.log(error);
+  }
 }
-
 
 /**
  *
@@ -71,26 +75,39 @@ async function getElbDnsName(elbObj){
  * conditions to test whether there is a targetgroup, loadbalancer name or no alb/elb attached to the service
  */
 
-async function describeServices(clusterName){
-	try {
-		const serviceNames = await listServices(clusterName)
-		const serviceDescriptions = await ecs.describeServices({services:serviceNames, cluster:clusterName}).promise()
-		return Promise.all(serviceDescriptions.services.map(async (service)=>({
-			serviceName: service.serviceName,
-			desiredCount: service.desiredCount,
-			createdAt: service.createdAt,
-			runningCount: service.runningCount,
-			pendingCount: service.pendingCount,
-			loadBalancer: {
-				// TODO if target group available get the associated load balancer
-				loadBalancerName:	service.loadBalancers[0] ? await getElbDnsName(service.loadBalancers) : `null`,
-				containerPort:	service.loadBalancers[0].containerPort,
-				albName:	service.loadBalancers[0].targetGroupArn ? await getTargetGroupAlb(service.loadBalancers[0].targetGroupArn) : `null`
-			},
-		})))
-	} catch (error) {
-		throw new Error(error)
-	}
+async function describeServices(clusterName) {
+  try {
+    const serviceNames = await listServices(clusterName);
+    const serviceDescriptions = await ecs
+      .describeServices({ services: serviceNames, cluster: clusterName })
+      .promise();
+    return Promise.all(
+      serviceDescriptions.services.map(async service => ({
+        serviceName: service.serviceName,
+        desiredCount: service.desiredCount,
+        createdAt: service.createdAt,
+        runningCount: service.runningCount,
+        pendingCount: service.pendingCount,
+        loadBalancer: {
+          loadBalancerName: service.loadBalancers[0]
+            ? await getElbDnsName(service.loadBalancers)
+            : `null`,
+          containerPort: service.loadBalancers[0].containerPort,
+          albName: service.loadBalancers[0].targetGroupArn
+            ? await getTargetGroupAlb(service.loadBalancers[0].targetGroupArn)
+            : `null`
+        }
+      }))
+    );
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+
+async function getEc2PrivateIp(instanceId){
+	const privateIp = await ec2.describeInstances({InstanceIds: [instanceId]}).promise()
+	return privateIp.Reservations[0].Instances[0].NetworkInterfaces[0].PrivateIpAddress
 }
 
 
@@ -99,33 +116,44 @@ async function describeServices(clusterName){
  * @param {string} clusterName
  * @returns {Array} list of registered container instances
  */
-async function listContainerInstances(clusterName){
-	try {
-		let containerInstances = await ecs.listContainerInstances({cluster:clusterName}).promise()
-		return containerInstances.containerInstanceArns
-	} catch (error) {
-		throw new Error(error)
-	}
+async function listContainerInstances(clusterName) {
+  try {
+    const containerInstances = await ecs
+      .listContainerInstances({ cluster: clusterName })
+			.promise();
+		//console.log(containerInstances.containerInstanceArns)
+		const containerInstanceDesc = await ecs
+			.describeContainerInstances({cluster:clusterName, containerInstances:containerInstances.containerInstanceArns})
+			.promise()
+		const containerInstancesWithIps = Promise.all(containerInstanceDesc.containerInstances.map(async el => {
+			console.log(`test ${el.ec2InstanceId}`)
+			const additional_info = {
+				privateIp: await getEc2PrivateIp(el.ec2InstanceId),
+				clusterName: clusterName
+			}
+			return Object.assign(el,additional_info)
+		}));
+    return containerInstancesWithIps;
+  } catch (error) {
+    throw new Error(error);
+  }
 }
 
 export async function main(event, context) {
-  const clusterName = event.pathParameters.name
-	const services = await listServices(clusterName)
-	const serviceDescriptions = await describeServices(clusterName)
-	const containerInstances = await listContainerInstances(clusterName)
-	// console.log(services)
-	// console.log(clusterName)
-	// console.log(containerInstances)
+  const clusterName = event.pathParameters.name;
+  const services = await listServices(clusterName);
+	const serviceDescriptions = await describeServices(clusterName);
 	console.log(serviceDescriptions)
+	const containerInstances = await listContainerInstances(clusterName);
+	console.log(containerInstances)
+	const combined = {
+		containerInstances,
+		serviceDescriptions
+	}
   try {
-    let clusterArn =
-      "arn:aws:ecs:us-east-1:174076265606:cluster/ContentPublishing_qa";
-    let clusterDetails = await ecs
-      .describeClusters({ clusters: [clusterArn] })
-      .promise();
     return {
       statusCode: 200,
-      body: JSON.stringify(clusterDetails)
+      body: JSON.stringify(combined)
     };
   } catch (error) {
     return {
